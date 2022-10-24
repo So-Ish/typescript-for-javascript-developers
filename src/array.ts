e6717edab67763391a3013a6ab0281288002cf67
SakuraXene <ishikawa.soma@fujitsu.com>
2022-10-24 16:27:05 +0900

    TypeScript basic practice

---
+++ src/array.ts
@@ -0,0 +1,16 @@
+export {};
+
+let numbers: number[] = [1, 2, 3];
+
+let number2: Array<number> = [1, 2, 3];
+
+let strings2: Array<string> = ['Toyko', 'Osaka', 'Kyoto'];
+
+let strgins: string[] = ['TypeScript', 'JavaScript', 'CoffeeScript'];
+
+let nijigenHairetsu: number[][] = [
+  [50, 100],
+  [150, 300],
+];
+
+let hairetsu: (string | number | boolean)[] = [1, false, 'Japan'];
--- src/boolean.ts
+++ src/boolean.ts
@@ -4,5 +4,5 @@ let name: string = 'TypeScript';
 
 let isFinished: boolean = true;
 isFinished = false;
-isFinished = 1; //エラー文 isFinished は boolean型
+// isFinished = 1;
 console.log({ isFinished });
