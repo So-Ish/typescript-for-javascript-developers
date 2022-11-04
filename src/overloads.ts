7b4b20743d14f4f773c35da238aa4b92fc142c6b
SakuraXene <ishikawa.soma@fujitsu.com>
2022-11-04 17:24:56 +0900

    add functions

---
+++ src/overloads.ts
@@ -0,0 +1,5 @@
+export {};
+
+function double(value: number): number {
+  return value * 2;
+}
---
+++ src/rest-parameters.ts
@@ -0,0 +1,13 @@
+export {};
+
+const reducer = (accumlator: number, currentValue: number) => {
+  console.log({ accumlator, currentValue });
+  return accumlator + currentValue;
+};
+
+const sum: (...values: number[]) => number = (...values: number[]): number => {
+  console.log(values);
+  return values.reduce(reducer);
+};
+
+console.log(sum(1, 2, 3, 4, 5));
